67dfbe537f00e36b6698310c4b94d8bb34e81465
ylli.dragusha <ylli.sonn@gmail.com>
2022-05-05 17:32:27 +0200

    DesktopAppPage & PaymentMethodsTest

--- src/test/java/com/phptravels/tests/DesktopAppTest.java
+++ src/test/java/com/phptravels/tests/DesktopAppTest.java
@@ -1,20 +1,24 @@
 package com.phptravels.tests;
 
 import com.phptravels.pages.DashboardPage;
+import com.phptravels.pages.DesktopAppPage;
 import com.phptravels.utilities.ConfigurationReader;
 import com.phptravels.utilities.Driver;
-import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class PaymentMethodsTest {
+public class DesktopAppTest {
 
     DashboardPage dashboardPage = new DashboardPage();
+    DesktopAppPage desktopAppPage = new DesktopAppPage();
 
     @Test
-    public void paymentMethodTest() {
+    public void desktopApptest() {
         Driver.getDriver().get(ConfigurationReader.getProperty("siteurl"));
-        dashboardPage.verifyElement("All the major e-payment gatways are accepted here including Paypal, Credit Card, Western Union, Skrill, Bitcoin, Transferwise.", dashboardPage.paymentMethods);
-    }
 
+        dashboardPage.desktopApp.click();
+
+        Assert.assertEquals(Driver.getDriver().getTitle(),"Desktop application - PHPTRAVELS");
+
+    }
 }
